5cbe206853926f44f23cffb28d95ddf0baa50d83
rnonnon <remi.nonnon@gmail.com>
2016-06-21 22:24:21 +0200

    improving view

---
+++ resources/assets/typescript/service/TripService.ts
@@ -0,0 +1,10 @@
+var app = angular.module("trip");
+
+class TripService {
+    static $inject = ['$http', '$q'];
+    public getTrip(id:number){
+
+    }
+}
+
+app.service("TripService", TripService);
